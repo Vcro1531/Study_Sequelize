d2bbfc1515b76e953f9421de3378a60fcf309dd5
KimJiMin <minmin4736@naver.com>
2021-10-11 22:56:03 +0900

    ⚡️ :: app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ nunjucks.configure('views', {
 sequelize
 	.sync({ force: false })
 	.then(() => {
-		console.log('데이터베이스 연결 성공');
+		console.log('DataBase Link Success');
 	})
 	.catch((err) => {
 		console.error(err);
